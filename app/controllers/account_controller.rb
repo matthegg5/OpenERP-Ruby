9f9e24a2abed5b5111bb2802a34226b9e030e0c4
saffron <matthew.heggie@googlemail.com>
2024-12-16 11:27:37 +0000

    login page working - authentication not configured yet

---
+++ app/controllers/account_controller.rb
@@ -0,0 +1,5 @@
+class AccountController < ApplicationController
+  def login
+    @title = "Login to OpenERP"
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
+  devise_for :users
+
+  # Custom route for login
+  devise_scope :user do
+    post "account/login", to: "devise/sessions#create", as: :account_session
+  end
+  get "account/login", to: "account#login", as: :login_page
   get "home/index"
   resources :companies
-  root 'home#index'           # For the Home link
-  get 'part', to: 'home#part'
-  get 'games', to: 'home#games'
-  get 'about', to: 'home#about'
-  #devise_for :users          # Devise routes
+  root "home#index"           # For the Home link
+  get "part", to: "home#part"
+  get "games", to: "home#games"
+  get "about", to: "home#about"
 end
