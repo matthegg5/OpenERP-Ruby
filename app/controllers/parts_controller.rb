7836b725290179cd66221ce2dc77b9c8f6d32481
matthegg5 <matthew.heggie@googlemail.com>
2025-03-18 21:55:44 +0000

    add authentication to the parts controller

--- app/controllers/parts_controller.rb
+++ app/controllers/parts_controller.rb
@@ -1,4 +1,5 @@
 class PartsController < ApplicationController
+before_action :authenticate_user!, only: [ :part, :create ]
   def index
     @title = "OpenERP - Part Maintenance"
     @current_company = "TEST"
