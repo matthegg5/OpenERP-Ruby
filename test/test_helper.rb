06907fd719080cecbd47c502e54465cfabdc2e48
saffron <matthew.heggie@googlemail.com>
2025-04-07 19:34:06 +0100

    attempt at testing - not working, Rails.application not found for capybara

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,14 +6,17 @@ require "selenium/webdriver"
 
 # Configure Capybara to use Selenium with Chrome
 Capybara.register_driver :chrome do |app|
-  options = Selenium::WebDriver::Chrome::Options.new
-  # options.add_argument("--headless")
-  options.add_argument("--disable-gpu")
-  options.add_argument("--no-sandbox")
-
-  Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
+  Capybara::Selenium::Driver.new app, browser: :chrome,
+    options: Selenium::WebDriver::Chrome::Options.new(args: %w[headless disable-gpu])
 end
 
 # Set the default driver for Capybara
 Capybara.default_driver = :chrome
 Capybara.javascript_driver = :chrome
+
+# Enable the Capybara driver for system tests
+Capybara.server = :puma
+
+
+# Automatically restart the database between tests (important for tests involving the database)
+ActiveRecord::Migration.maintain_test_schema!
